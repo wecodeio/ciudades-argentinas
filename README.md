1d4e3efea69baee72bdc6d62ea4775efd2107c71
Cristian Rasch <cristianrasch@gmail.com>
2013-08-04 23:33:15 -0300

    Scraper y JSON de prueba

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ ciudades-argentinas
 ===================
 
 Scraper de ciudades argentinas basado en los datos publicados en http://www.migraciones.gov.ar/
+
+Modo de uso
+-----------
+
+1. bundle install
+
+2. chmod +x scraper.rb
+
+3. ./scraper.rb
---
+++ scraper.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require "open-uri"
+require "json"
+require "nokogiri"
+
+class Ciudad < Struct.new(:id, :nombre)
+  def to_h
+    { id: id, nombre: nombre }
+  end
+end
+
+class Provincia
+  attr_reader :id, :nombre, :ciudades
+  attr_writer :ciudades
+
+  def initialize(id, nombre)
+    @id, @nombre = id.to_i, nombre
+    @ciudades = []
+  end
+
+  def to_h
+    { id: id, nombre: nombre, ciudades: ciudades.map(&:to_h) }
+  end
+end
+
+doc = Nokogiri::HTML(open("http://www.migraciones.gov.ar/tarjeta/index2TES.php?idioma=ESPA&tar=TES"))
+
+provincias = doc.css("#provincias option").reject { |option| option["value"].empty? }
+provincias.map! { |option| Provincia.new(option["value"].to_i, option.text) }
+
+provincias.each do |provincia|
+  doc = Nokogiri::HTML(open("http://www.migraciones.gov.ar/tarjeta/ajaxLocalidades.php?provincia=#{provincia.id}"))
+  ciudades = doc.css("option")
+  provincia.ciudades = ciudades.map { |option| Ciudad.new(option["value"], option.text) }
+end
+
+File.open("ciudades-argentinas.json", "w") do |f|
+  f << JSON.generate(provincias.map!(&:to_h))
+end
